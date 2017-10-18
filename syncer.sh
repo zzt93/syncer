5cfa10d2398cea2a037c991ec598aa09970b424f
zzt <zengzt93@gmail.com>
2017-10-18 10:24:23 +0800

    update startup script

--- syncer.sh
+++ syncer.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export BUILD_ID=dontKillMe
+fuser -k -n tcp 5006
 nohup java "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006" -jar ./target/syncer-1.0-SNAPSHOT.jar --pipelineConfig=$1 &
