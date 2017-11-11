963fe35e68dedcc5a5040023b61b2205a28ffb15
zzt <zengzt93@gmail.com>
2017-11-11 16:42:40 +0800

    update readme and todo.

--- src/main/java/com/github/zzt93/syncer/common/event/DeleteRowsEvent.java
+++ src/main/java/com/github/zzt93/syncer/common/event/DeleteRowsEvent.java
@@ -18,7 +18,7 @@ public class DeleteRowsEvent extends RowsEvent {
   public DeleteRowsEvent(Event tableMap, DeleteRowsEventData deleteRowsEventData,
       Map<Integer, String> indexToName, Set<Integer> primaryKeys) {
     super(tableMap, indexToName, primaryKeys);
-    // TODO 17/10/25 only keep id
+    // TODO 17/10/25 only keep non-null field: id, partition key
     BitSet includedColumns = deleteRowsEventData.getIncludedColumns();
     List<Serializable[]> rows = deleteRowsEventData.getRows();
     for (Serializable[] row : rows) {
