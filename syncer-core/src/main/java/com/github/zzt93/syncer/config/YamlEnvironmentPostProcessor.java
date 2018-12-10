5e73bcb9cb7348f42bebb337362fff8c4fe7752c
zzt <zengzt93@gmail.com>
2018-12-10 19:16:52 +0800

    fix import of MysqlMasterConnector.java; add directory support for consumer config.

--- syncer-core/src/main/java/com/github/zzt93/syncer/config/YamlEnvironmentPostProcessor.java
+++ syncer-core/src/main/java/com/github/zzt93/syncer/config/YamlEnvironmentPostProcessor.java
@@ -17,9 +17,11 @@ import org.yaml.snakeyaml.Yaml;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
 
@@ -44,21 +46,39 @@ public class YamlEnvironmentPostProcessor implements EnvironmentPostProcessor {
           "No producer config file specified, try '--" + PRODUCER_CONFIG
               + "=producer.yml");
     }
-    String pipelineNames = environment.getProperty(CONSUMER_CONFIG);
-    if (pipelineNames == null) {
-      throw new IllegalArgumentException(
-          "No consumer config file specified, try '--" + CONSUMER_CONFIG
-              + "=sample.yml,sample2.yml'");
-    }
+    Set<String> filePaths = getFilePaths(environment);
     String configFile = environment.getProperty(CONFIG);
     if (configFile == null) {
       configFile = "config.yml";
     }
     environment.getPropertySources().addLast(loadYaml(producer));
     environment.getPropertySources().addLast(loadYaml(configFile));
-    for (String fileName : pipelineNames.split(",")) {
+    for (String fileName : filePaths) {
       configs.add(initPipelines(fileName, environment));
     }
+  }
+
+  private Set<String> getFilePaths(ConfigurableEnvironment environment) {
+    String pipelineNames = environment.getProperty(CONSUMER_CONFIG);
+    if (pipelineNames == null) {
+      throw new IllegalArgumentException(
+          "No consumer config file specified, try '--" + CONSUMER_CONFIG
+              + "=sample.yml,sample2.yml'");
+    }
+    Set<String> res = new HashSet<>();
+    for (String name : pipelineNames.split(",")) {
+      Path path = Paths.get(name);
+      if (Files.isDirectory(path)) {
+        try (DirectoryStream<Path> paths = Files.newDirectoryStream(path, "*.{yml,yaml}")) {
+          paths.iterator().forEachRemaining(p->res.add(p.toString()));
+        } catch (IOException e) {
+          logger.error("Fail to load/parse yml file", e);
+        }
+      } else {
+        res.add(name);
+      }
+    }
+    return res;
   }
 
   private ConsumerConfig initPipelines(String fileName,
