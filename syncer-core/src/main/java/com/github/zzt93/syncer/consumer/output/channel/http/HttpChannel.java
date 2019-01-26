def3e4b2856de7b93bdf5f20af326c2d1c26acf3
zzt <zengzt93@gmail.com>
2019-01-26 20:53:34 +0800

    finish impl netty client as http channel.

--- syncer-core/src/main/java/com/github/zzt93/syncer/consumer/output/channel/http/HttpChannel.java
+++ syncer-core/src/main/java/com/github/zzt93/syncer/consumer/output/channel/http/HttpChannel.java
@@ -5,17 +5,16 @@ import com.github.zzt93.syncer.config.common.HttpConnection;
 import com.github.zzt93.syncer.consumer.ack.Ack;
 import com.github.zzt93.syncer.consumer.output.channel.OutputChannel;
 import com.github.zzt93.syncer.consumer.output.mapper.KVMapper;
+import com.google.gson.Gson;
+import io.netty.handler.codec.http.HttpMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.springframework.http.HttpMethod.*;
+import static io.netty.handler.codec.http.HttpMethod.*;
+
 
 /**
  * Should be thread safe
@@ -24,18 +23,19 @@ import static org.springframework.http.HttpMethod.*;
  */
 public class HttpChannel implements OutputChannel {
 
+  private static final Gson gson = new Gson();
   private final Logger logger = LoggerFactory.getLogger(HttpChannel.class);
-  private final RestTemplate restTemplate;
   private final String connection;
   private final KVMapper mapper;
   private final Ack ack;
   private final String id;
+  private final NettyClient nettyClient;
 
   public HttpChannel(HttpConnection connection, Map<String, Object> jsonMapper,
       Ack ack) {
     this.ack = ack;
-    this.restTemplate = new RestTemplate();
     this.connection = connection.toConnectionUrl(null);
+    nettyClient = new NettyClient(connection);
     id = connection.connectionIdentifier();
     this.mapper = new KVMapper(jsonMapper);
   }
@@ -47,27 +47,26 @@ public class HttpChannel implements OutputChannel {
    * @param event the data from filter module
    */
   @Override
-  public boolean output(SyncData event) {
+  public boolean output(SyncData event) throws InterruptedException {
     // TODO 17/9/22 add batch worker
     // TODO 18/6/6 add ack
     HashMap<String, Object> res = mapper.map(event);
     logger.debug("Mapping table row {} to {}", event.getFields(), res);
     switch (event.getType()) {
       case UPDATE:
-        return execute(res, POST).is2xxSuccessful();
+        return execute(res, POST);
       case DELETE:
-        return execute(res, DELETE).is2xxSuccessful();
+        return execute(res, DELETE);
       case WRITE:
-        return execute(res, PUT).is2xxSuccessful();
+        return execute(res, PUT);
       default:
         return false;
     }
   }
 
 
-  private HttpStatus execute(HashMap<String, Object> res, HttpMethod method) {
-    return restTemplate.exchange(connection, method, new HttpEntity<Map>(res), Object.class)
-        .getStatusCode();
+  private boolean execute(HashMap<String, Object> res, HttpMethod method) throws InterruptedException {
+    return nettyClient.write(method, gson.toJson(res));
   }
 
   @Override
@@ -79,7 +78,7 @@ public class HttpChannel implements OutputChannel {
 
   @Override
   public void close() {
-
+    nettyClient.close();
   }
 
   @Override
