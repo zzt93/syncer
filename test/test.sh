2ed756789864baf3588d45615d45d4109b4a55e6
zzt <zengzt93@gmail.com>
2019-02-21 08:50:48 +0800

    make mysql enable binlog. fix some mysql query.

--- test/test.sh
+++ test/test.sh
@@ -3,13 +3,17 @@ config=$1
 env=$2
 num=$3
 
+
+if [[ -z "${config}" ]]; then
+    config="yaml"
+fi
+if [[ -z "${env}" ]]; then
+    env="mysql"
+fi
 if [[ -z "${num}" ]]; then
     num=100
 fi
 
-if [[ -z "${env}" ]]; then
-    env="mysql"
-fi
 
 # package syncer
 mvn package
@@ -31,8 +35,8 @@ if [[ $config = "yaml" || ${config} = "yml" ]]; then
 elif [[ $config = "code" ]]; then
     cp config/correctness-consumer-code.yml config/consumer/correctness-consumer-code.yml
 else
-    echo "prepare code env"
-    cp config/correctness-consumer-code.yml config/consumer/correctness-consumer-code.yml
+    echo ""
+    exit 1
 fi
 cd ..
 
@@ -41,22 +45,22 @@ cd ..
 # start env by docker-compose
 # init data
 echo "prepare $env env"
-bash setup.sh $num $env
+bash setup.sh ${num} ${env}
 
 
-all=`docker-compose -f mysql.yml exec mysql mysql -uroot -proot -N -B -e 'select count(*) from test.news' | grep -o "[0-9]*"`
+all=`docker-compose -f ${env}.yml exec mysql_0 mysql -uroot -proot -N -B -e 'select count(*) from test_0.news' | grep -o "[0-9]*"`
 echo "test.news: $all"
 
 # Then
 # query ES count
 
-c1=`curl -s -X GET "localhost:49200/test/news/_count" -H 'Content-Type: application/json'`
+c1=`curl -s -X GET "localhost:49200/test*/news/_count" -H 'Content-Type: application/json'`
 echo "es: $c1"
 if [[ ${c1} -eq "$all" ]];then
     exit 1
 fi
 
-c2=`curl -s -X GET "localhost:49200/test/news/_count" -H 'Content-Type: application/json' -d'
+c2=`curl -s -X GET "localhost:49200/test*/news/_count" -H 'Content-Type: application/json' -d'
 {
     "query" : {
         "term" : { "user" : "kimchy" }
@@ -65,7 +69,7 @@ c2=`curl -s -X GET "localhost:49200/test/news/_count" -H 'Content-Type: applicat
 '`
 
 # query mysql count
-d1=`docker-compose -f mysql.yml exec mysql mysql -uroot -proot -N -B -e 'select count(*) from test.news_bak' | grep -o "[0-9]*"`
+d1=`docker-compose -f $env.yml exec mysql_0 mysql -uroot -proot -N -B -e 'select count(*) from test_0.news_bak' | grep -o "[0-9]*"`
 echo "test.news_bak: $d1"
 
 if [[ ${d1} -eq "$all" ]];then
