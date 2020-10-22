7e5badd0c276bbc9f68bccd4007796307a841878
zzt <zengzt93@gmail.com>
2020-10-22 09:58:48 +0800

    remove JsonKafkaDeserializer JsonObject type adapter.

--- syncer-data/src/main/java/com/github/zzt93/syncer/data/kafka/JsonKafkaDeserializer.java
+++ syncer-data/src/main/java/com/github/zzt93/syncer/data/kafka/JsonKafkaDeserializer.java
@@ -3,8 +3,6 @@ package com.github.zzt93.syncer.data.kafka;
 import com.github.zzt93.syncer.data.SimpleEventType;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonObject;
 import org.apache.kafka.common.serialization.Deserializer;
 
 import java.util.Map;
@@ -14,7 +12,6 @@ import java.util.Map;
  */
 public class JsonKafkaDeserializer implements Deserializer<JsonSyncResult> {
   private static final Gson gson = new GsonBuilder()
-      .registerTypeAdapter(JsonObject.class, (JsonDeserializer<JsonObject>) (jsonElement, type, jsonDeserializationContext) -> jsonElement.isJsonObject() ? jsonElement.getAsJsonObject() : new JsonObject())
       .registerTypeHierarchyAdapter(SimpleEventType.class, SimpleEventType.defaultDeserializer)
       .create();
 
