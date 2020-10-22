6922bc1f8d4694de0ad48447fa1e717effb4518d
zzt <zengzt93@gmail.com>
2020-10-22 09:34:33 +0800

    format JsonKafkaDeserializer.

--- syncer-data/src/main/java/com/github/zzt93/syncer/data/kafka/JsonKafkaDeserializer.java
+++ syncer-data/src/main/java/com/github/zzt93/syncer/data/kafka/JsonKafkaDeserializer.java
@@ -1,32 +1,34 @@
 package com.github.zzt93.syncer.data.kafka;
 
 import com.github.zzt93.syncer.data.SimpleEventType;
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonObject;
 import org.apache.kafka.common.serialization.Deserializer;
 
-import java.lang.reflect.Type;
 import java.util.Map;
 
 /**
  * @author zzt
  */
 public class JsonKafkaDeserializer implements Deserializer<JsonSyncResult> {
-	private static final Gson gson = new GsonBuilder()
-			.registerTypeAdapter(JsonObject.class, (JsonDeserializer<JsonObject>) (jsonElement, type, jsonDeserializationContext) -> jsonElement.isJsonObject() ? jsonElement.getAsJsonObject() : new JsonObject())
-			.registerTypeHierarchyAdapter(SimpleEventType.class, SimpleEventType.defaultDeserializer)
-			.create();
+  private static final Gson gson = new GsonBuilder()
+      .registerTypeAdapter(JsonObject.class, (JsonDeserializer<JsonObject>) (jsonElement, type, jsonDeserializationContext) -> jsonElement.isJsonObject() ? jsonElement.getAsJsonObject() : new JsonObject())
+      .registerTypeHierarchyAdapter(SimpleEventType.class, SimpleEventType.defaultDeserializer)
+      .create();
 
 
-	@Override
-	public void configure(Map<String, ?> map, boolean b) {
-	}
+  @Override
+  public void configure(Map<String, ?> map, boolean b) {
+  }
 
-	@Override
-	public JsonSyncResult deserialize(String s, byte[] bytes) {
-		return gson.fromJson(new String(bytes), JsonSyncResult.class);
-	}
+  @Override
+  public JsonSyncResult deserialize(String s, byte[] bytes) {
+    return gson.fromJson(new String(bytes), JsonSyncResult.class);
+  }
 
-	@Override
-	public void close() {
-	}
+  @Override
+  public void close() {
+  }
 }
