fe859ccf0005d1a0be11f648809a2749bcd40734
YYT23333 <41135056+YYT23333@users.noreply.github.com>
2020-10-22 09:13:16 +0800

    add id in fields (#13)
    * fix id
    
    * change extras to JsonObject
    
    Co-authored-by: 袁玉婷 <yuanyt@superid.cn>

--- syncer-data/src/main/java/com/github/zzt93/syncer/data/kafka/JsonKafkaDeserializer.java
+++ syncer-data/src/main/java/com/github/zzt93/syncer/data/kafka/JsonKafkaDeserializer.java
@@ -11,31 +11,22 @@ import java.util.Map;
  * @author zzt
  */
 public class JsonKafkaDeserializer implements Deserializer<JsonSyncResult> {
-  private static final Gson gson = new GsonBuilder()
-      .registerTypeAdapter(String.class, new MapDeserializer())
-      .registerTypeHierarchyAdapter(SimpleEventType.class, SimpleEventType.defaultDeserializer)
-      .create();
+	private static final Gson gson = new GsonBuilder()
+			.registerTypeAdapter(JsonObject.class, (JsonDeserializer<JsonObject>) (jsonElement, type, jsonDeserializationContext) -> jsonElement.isJsonObject() ? jsonElement.getAsJsonObject() : new JsonObject())
+			.registerTypeHierarchyAdapter(SimpleEventType.class, SimpleEventType.defaultDeserializer)
+			.create();
 
-  private static class MapDeserializer implements JsonDeserializer<String> {
-    public String deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-        throws JsonParseException {
-      if (json.isJsonObject()) {
-        return json.toString();
-      }
-      return json.getAsString();
-    }
-  }
 
-  @Override
-  public void configure(Map<String, ?> map, boolean b) {
-  }
+	@Override
+	public void configure(Map<String, ?> map, boolean b) {
+	}
 
-  @Override
-  public JsonSyncResult deserialize(String s, byte[] bytes) {
-    return gson.fromJson(new String(bytes), JsonSyncResult.class);
-  }
+	@Override
+	public JsonSyncResult deserialize(String s, byte[] bytes) {
+		return gson.fromJson(new String(bytes), JsonSyncResult.class);
+	}
 
-  @Override
-  public void close() {
-  }
+	@Override
+	public void close() {
+	}
 }
