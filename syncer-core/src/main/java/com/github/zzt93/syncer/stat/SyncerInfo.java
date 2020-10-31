0df9f562ddafa5bf2066c604d8298310e21b406f
zzt <zengzt93@gmail.com>
2020-10-31 15:44:05 +0800

    add syncer instanceId impl.

--- syncer-core/src/main/java/com/github/zzt93/syncer/stat/SyncerInfo.java
+++ syncer-core/src/main/java/com/github/zzt93/syncer/stat/SyncerInfo.java
@@ -3,6 +3,8 @@ package com.github.zzt93.syncer.stat;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.UUID;
+
 @Getter
 @ToString
 public class SyncerInfo {
@@ -17,6 +19,7 @@ public class SyncerInfo {
 	}
 
 	/**
+	 * {@link UUID} version 1 or version 4 (https://www.ietf.org/rfc/rfc4122.txt)
 	 * <ul>
 	 *   <li>Unique if start in different machine</li>
 	 *   <li>Unique if start multiple instances in same machine at different time</li>
@@ -28,10 +31,12 @@ public class SyncerInfo {
 	 *   machineId | time | random
 	 *   -------------------------
 	 * </pre>
+	 * <a href="https://github.com/elastic/elasticsearch/blob/fc8c6dd56d602b4a62ee1ff484f00caab92dc6e2/server/src/main/java/org/elasticsearch/env/NodeEnvironment.java#L527>ES node id generation</a>
+	 * <a href="https://github.com/elastic/elasticsearch/blob/master/server/src/main/java/org/elasticsearch/common/RandomBasedUUIDGenerator.java">ES impl</a>
 	 * @return syncer instanceId
 	 */
 	private static String generateInstanceId() {
-		return null;
+		return UUID.randomUUID().toString();
 	}
 
 }
